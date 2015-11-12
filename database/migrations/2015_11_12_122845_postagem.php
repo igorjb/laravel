6186143ef23e1ce4a0e769521396ae14edc8c058
igor.brilhante <igorjosebrilhante@gmail.com>
2015-11-12 10:01:56 -0300

    criando migrations.

---
+++ database/migrations/2015_11_12_122845_postagem.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Postagem extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('postagens', function($table){
+            $table->increments('id_postagem');
+            $table->string('titulo', 100);
+            $table->text('conteudo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('postagens');
+    }
+}
