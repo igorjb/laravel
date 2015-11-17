2474ec55a97e949e7fcd0aecded18a47b53cb439
Igor Brilhante <igorjosebrilhante@gmail.com>
2015-11-17 09:30:33 -0300

    Commit teste

---
+++ app/PostagemDados.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PostagemDados extends Model
+{
+    //
+    protected $table = 'postagens';
+
+	protected $primaryKey = 'id_postagem';
+}
